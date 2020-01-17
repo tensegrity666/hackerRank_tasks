0b323e789e4e349ff9ab3528642d203559b9aca8
Roman Yakovlev <tensegrity666@eicloud.com>
2020-01-17 22:45:11 +0300

    Task completed

--- loops.js
+++ loops.js
@@ -1,17 +1,26 @@
 'Use strict';
 
-function vowelsAndConsonants(s) {
-  const output = [];
-  const vowels = 'aeiou';
-  for (let char of s) {
-    if (vowels.includes(char)) {
-      output.push(char);
-      output.sort();
-    };
-  };
-  return console.log(output)
-}
-
 const s = 'javascriptloops';
+const VOWELS = 'aeiou';
+
+
+function vowelsAndConsonants(s) {
+  const output_v = [];
+  const output = [];
+  let arr = [];
+
+  for (let char of s) {
+    if (VOWELS.includes(char)) {
+      output_v.sort().push(char);
+    };
+    if (!VOWELS.includes(char)) {
+      output.sort().push(char)
+    };
+    arr = output_v.concat(output).join('');
+  };
+  for (let n of arr) {
+    console.log(n);
+  }
+}
 
 vowelsAndConsonants(s);
