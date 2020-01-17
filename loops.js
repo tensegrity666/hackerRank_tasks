08feb0586afd8580cbd05d67734fae9eab61cb91
Roman Yakovlev <tensegrity666@eicloud.com>
2020-01-17 23:46:30 +0300

    Second part of task

--- loops.js
+++ loops.js
@@ -1,25 +1,34 @@
 'Use strict';
 
-const s = 'javascriptloops';
+const s = 'learnjavascriptloopsinhackerrankweekofcode';
 
 function vowelsAndConsonants(s) {
   const VOWELS = 'aeiou';
   const output_v = [];
   const output = [];
   let arr = [];
+  const TRUNC = '{-truncated-}';
 
   for (let char of s) {
     if (VOWELS.includes(char)) {
       output_v.push(char);
     };
+
     if (!VOWELS.includes(char)) {
-      output.push(char)
+      output.push(char);
     };
-    arr = output_v.concat(output).join('');
   };
-  for (let n of arr) {
-    console.log(n);
+
+  arr = output_v.concat(output);
+
+  if (arr.length >= 40) {
+    arr = arr.slice(0, 20).join('\n').concat(TRUNC);
+  } else {
+    arr = arr.slice(0, 20).join('\n');
   }
+
+  console.log(arr);
+  return arr;
 }
 
 vowelsAndConsonants(s);
