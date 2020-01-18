6eaed7148b83daa381db3904854fbf0b7234eb59
Roman Yakovlev <tensegrity666@eicloud.com>
2020-01-18 21:37:31 +0300

    Adds some fixes

--- functions.js
+++ functions.js
@@ -1,9 +1,11 @@
+'Use strict';
+
 function factorial(n) {
-  if (n === 1) {
+  if ((n === 1) || (n === 2)) {
     return n;
   } else {
     return n * factorial(n - 1);
-  }
-}
+  };
+};
 
-console.log(factorial(4));
+console.log(factorial(5)); //120
