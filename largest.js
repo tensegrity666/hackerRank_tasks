90358cd8800d545f316ec72afc06e68e2096ec81
Roman Yakovlev <tensegrity666@eicloud.com>
2020-01-18 23:47:19 +0300

    Adds search of second largest

--- largest.js
+++ largest.js
@@ -15,9 +15,9 @@ function getSecondLargest(nums) {
   };
   console.log(largest);
 
-  for (let j = 0; j < nums.length; ++j) {
-    if (nums[j] > nums[j + 1] && nums[j] < largest) {
-      number = nums[j];
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] < largest) {
+      number = nums[i];
     };
   };
   console.log(number);
